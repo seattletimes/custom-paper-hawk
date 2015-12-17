89f875c184f505629912b678a8d00be2a14a2551
Thomas Wilburn <news@thomaswilburn.net>
2015-12-17 12:49:47 -0800

    Refactor, consolidate controls a bit

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,6 @@
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
-/*
-Rendering plan for printable paper hawk:
-- get face zone from the SVG, create a clip path for it
-- is it possible to reference a canvas image from SVG?
-- Add the ability to change the face fill to different colors
-- On print, render the SVG to a big canvas and then dump to data URI for downloading?
-- 
-*/
-
 var qsa = s => Array.prototype.slice.call(document.querySelectorAll(s));
 var $ = require("./savage");
 var SVGCamera = require("./camera");
@@ -23,9 +14,10 @@ var selfie = new SelfieCamera();
 var faceCanvas = document.querySelector("canvas.face");
 var faceContext = faceCanvas.getContext("2d");
 
-var portrait = new Image();
-portrait.src = "./assets/grump.jpg";
+var mug = new Image();
+mug.src = "./assets/grump.jpg";
 
+// Rendering for the mug
 var pos = {
   x: faceCanvas.width / 2,
   y: faceCanvas.height / 2,
@@ -36,6 +28,45 @@ var pos = {
   width: 1,
   height: 1
 };
+
+var facePath = document.querySelector("#face .st61");
+var svgContainer = document.querySelector(".canvas-inner");
+
+var positionFaceCanvas = function() {
+  var faceBounds = facePath.getBoundingClientRect();
+  var containerBounds = svgContainer.getBoundingClientRect();
+  faceCanvas.style.width = faceBounds.width / containerBounds.width * 100 + "%";
+  faceCanvas.style.height = faceBounds.height / containerBounds.height * 100 + "%";
+  faceCanvas.style.left = (faceBounds.left - containerBounds.left) / containerBounds.width * 100 + "%";
+  faceCanvas.style.top = (faceBounds.top - containerBounds.top) / containerBounds.height * 100 + "%";
+  faceCanvas.width = faceCanvas.offsetWidth;
+  faceCanvas.height = faceCanvas.offsetHeight;
+  faceContext.arc(faceCanvas.width / 2, faceCanvas.height / 2, faceCanvas.width / 2, 0, Math.PI * 2);
+  faceContext.clip();
+};
+
+var drawFace = function() {
+  positionFaceCanvas() //will also clear the canvas
+  var {x, dx, y, dy, scale, pinch, width, height} = pos;
+  width = width * pinch * scale;
+  height = height * pinch * scale;
+  x = x + dx - width / 2;
+  y = y + dy - height / 2;
+  faceContext.drawImage(mug, x, y, width, height);
+}
+drawFace();
+
+mug.onload = function() {
+  var w = mug.naturalWidth || mug.height;
+  var h = mug.naturalHeight || mug.height;
+  pos.width = w;
+  pos.height = h;
+  pos.x = faceCanvas.width / 2;
+  pos.y = faceCanvas.height / 2;
+  drawFace();
+}
+
+//touch controls
 
 var mc = new Hammer(faceCanvas);
 //enable all panning, pinch gestures
@@ -62,24 +93,9 @@ mc.on("pinchend", function() {
   pos.pinch = 1;
 });
 
-var drawFace = function() {
-  var {x, dx, y, dy, scale, pinch, width, height} = pos;
-  width = width * pinch * scale;
-  height = height * pinch * scale;
-  x = x + dx - width / 2;
-  y = y + dy - height / 2;
-  faceContext.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
-  faceContext.drawImage(portrait, x, y, width, height);
-}
-
-portrait.onload = function() {
-  pos.width = portrait.width;
-  pos.height = portrait.height;
-  drawFace();
-}
-
-var onWheelZoom = function(e) {
-  var el = e.target;
+// Mouse zoom controls
+var onZoom = function(e) {
+  var el = e.currentTarget;
   if (el.classList.contains("in")) {
     pos.scale *= 1.1;
   } else {
@@ -88,7 +104,7 @@ var onWheelZoom = function(e) {
   drawFace();
 };
 
-qsa(".zoom").forEach(el => el.addEventListener("click", onWheelZoom));
+qsa(".zoom").forEach(el => el.addEventListener("click", onZoom));
 
 faceCanvas.addEventListener("wheel", function(e) {
   e.preventDefault();
@@ -100,40 +116,25 @@ faceCanvas.addEventListener("wheel", function(e) {
   drawFace();
 });
 
+//load custom mugs from files or webcams
+
 var fileInput = document.querySelector("#upload-image");
 fileInput.addEventListener("change", function(e) {
   var reader = new FileReader();
   reader.addEventListener("load", function() {
-    portrait.src = reader.result;
-    portrait.onload = drawFace;
+    mug.src = reader.result;
+    mug.onload = drawFace;
   });
   reader.readAsDataURL(fileInput.files[0]);
 });
 
-var onResize = function() {
-  var faceBounds = document.querySelector("#face .st61").getBoundingClientRect();
-  var containerBounds = document.querySelector(".canvas-inner").getBoundingClientRect();
-  faceCanvas.style.width = faceBounds.width / containerBounds.width * 100 + "%";
-  faceCanvas.style.height = faceBounds.height / containerBounds.height * 100 + "%";
-  faceCanvas.style.left = (faceBounds.left - containerBounds.left) / containerBounds.width * 100 + "%";
-  faceCanvas.style.top = (faceBounds.top - containerBounds.top) / containerBounds.height * 100 + "%";
-  faceCanvas.width = faceCanvas.offsetWidth;
-  faceCanvas.height = faceCanvas.offsetHeight;
-  faceContext.arc(faceCanvas.width / 2, faceCanvas.height / 2, faceCanvas.width / 2, 0, Math.PI * 2);
-  faceContext.clip();
-  pos.x = faceCanvas.width / 2;
-  pos.y = faceCanvas.height / 2;
-  drawFace();
-};
-
-window.addEventListener("resize", onResize);
-onResize();
+window.addEventListener("resize", drawFace);
 
 document.querySelector(".take-selfie").addEventListener("click", function() {
   selfie.open(function(image) {
-    portrait = image;
-    pos.width = portrait.width;
-    pos.height = portrait.height;
+    mug = image;
+    pos.width = mug.width;
+    pos.height = mug.height;
     pos.x = faceCanvas.width / 2;
     pos.y = faceCanvas.width / 2;
     pos.scale = 1;
@@ -141,18 +142,20 @@ document.querySelector(".take-selfie").addEventListener("click", function() {
   })
 });
 
+// UI for skin tones
+
 var setShade = function(shade) {
   $("#face rect, #face path, #face polygon").style({
     fill: shade
   });
 }
 
-var onSkinChange = function() {
+var useEmojiSkin = function() {
   var shade = this.getAttribute("data-shade");
   setShade(shade);
 };
 
-qsa(".skin-tones .skin").forEach(el => el.addEventListener("click", onSkinChange));
+qsa(".skin-tones .emoji").forEach(el => el.addEventListener("click", useEmojiSkin));
 
 document.querySelector(".skin-tones .dropper").addEventListener("click", function() {
   var clickImage = function(e) {
